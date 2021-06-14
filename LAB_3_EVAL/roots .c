9361bcb3520a9814c480d7c86a5f2c44544d15c1
tusharcs20 <83938377+tusharcs20@users.noreply.github.com>
2021-06-14 09:57:09 +0530

    Update roots .c

--- LAB_3_EVAL/roots .c
+++ LAB_3_EVAL/roots .c
@@ -1,26 +1,32 @@
-#include <stdio.h>
 #include <math.h>
-int main()
-{
-  int a, b, c, d;
-  double root1, root2;
+#include <stdio.h>
+int main() {
+    double a, b, c, discriminant, root1, root2, realPart, imagPart;
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
 
-  printf("Enter a, b and c where a*x*x + b*x + c = 0\n");
-  scanf("%d%d%d", &a, &b, &c);
+    discriminant = b * b - 4 * a * c;
 
-  d = b*b - 4*a*c;
 
-  if (d < 0) {
-    printf("First root = %.2lf + i%.2lf\n", -b/(double)(2*a), sqrt(-d)/(2*a));
-    printf("Second root = %.2lf - i%.2lf\n", -b/(double)(2*a), sqrt(-d)/(2*a));
-  }
-  else { 
-    root1 = (-b + sqrt(d))/(2*a);
-    root2 = (-b - sqrt(d))/(2*a);
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("root1 = %.2lf and root2 = %.2lf; \n", root1, root2);
+        printf("roots are real and unequal");
+    }
 
-    printf("First root = %.2lf\n", root1);
-    printf("Second root = %.2lf\n", root2);
-  }
+    else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        printf("root1 = root2 = %.2lf;\n", root1);
+        printf("roots are real and equal");
+    }
 
-  return 0;
+    else {
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-discriminant) / (2 * a);
+        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi ; \n", realPart, imagPart, realPart, imagPart);
+        printf("roots are imaginary");
+    }
+
+    return 0;
 }
