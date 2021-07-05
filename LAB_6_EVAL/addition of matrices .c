eca16a913c486976581bde7c55a8be6da69523d3
tusharcs20 <83938377+tusharcs20@users.noreply.github.com>
2021-07-05 11:07:15 +0530

    Update addition of matrices .c

--- LAB_6_EVAL/addition of matrices .c
+++ LAB_6_EVAL/addition of matrices .c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <conio.h>
+int x,y;
 void main()
+
 {
-	int a[2][3],b[2][3],c[2][3],i,j;
-// x and y instead of 2 and 3
+    printf("enter the size of matrix");
+    printf("\n enter the number of rows");
+    scanf("%d",&x);
+    printf("\n enter the number of columns");
+    scanf("%d",&y);
+    int a[x][y],b[x][y],c[x][y],i,j;
+
 	printf("\nENTER VALUES FOR MATRIX A:\n");
-	for(i=0;i<2;i++)
-		for(j=0;j<3;j++)
+	for(i=0;i<x;i++)
+		for(j=0;j<y;j++)
 			scanf("%d",&a[i][j]);
 	printf("\nENTER VALUES FOR MATRIX B:\n");
-	for(i=0;i<2;i++)
-		for(j=0;j<3;j++)
+	for(i=0;i<x;i++)
+		for(j=0;j<y;j++)
 			scanf("%d",&b[i][j]);
-	for(i=0;i<2;i++)
-		for(j=0;j<3;j++)
+	for(i=0;i<x;i++)
+		for(j=0;j<y;j++)
 			c[i][j]=a[i][j]+b[i][j];
 	printf("\nTHE VALUES OF MATRIX C ARE:\n");
-	for(i=0;i<2;i++)
+	for(i=0;i<x;i++)
         {
-		for(j=0;j<3;j++)
+		for(j=0;j<y;j++)
 			printf("%5d",c[i][j]);
 		printf("\n");
 	}
